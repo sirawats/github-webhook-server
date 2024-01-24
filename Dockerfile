7e13353ce1c936a7871e8627d4478a540a46e5b6
Sirawat S <21076861+leossok@users.noreply.github.com>
2024-01-24 14:09:11 +0700

    Merge dev : tested (#1)
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix
    
    * TBD: fix

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
 FROM python:3.11-slim-buster
 
+# INSTALL
+RUN apt-get -y update
+RUN apt-get -y install git
+
+# CLEANUP
+RUN apt-get clean
+
 # Set the working directory in the container to /app
 WORKDIR /app
 
@@ -7,7 +14,7 @@ WORKDIR /app
 ADD . /app
 
 # Install any needed packages specified in requirements.txt
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r /app/requirements.txt
 
 # Run app.py when the container launches
 CMD ["python", "/app/webhook_server.py"]
--- webhook_server.py
+++ webhook_server.py
@@ -1,7 +1,34 @@
 import click
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request
 from subprocess import Popen, PIPE
+import hashlib
+import hmac
+import os
+import logging
+
+
+# Config Logging
+logger = logging.Logger("default_webhook_server_log")
+
+
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+
+    Raise and return 403 if not authorized.
+
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
+    hash_object = hmac.new(secret_token.encode("utf-8"), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise HTTPException(status_code=403, detail="Request signatures didn't match!")
+
 
 app = FastAPI()
 command = ["echo 'nothing to execute.'"]
@@ -14,19 +41,43 @@ async def root():
 
 @app.post("/webhook")
 async def webhook(request: Request):
-    print("HEADERS:")
-    print(request.headers)
-    print("JSON:")
-    print(request.json())
-    return {"message": "Hello World"}
+    logger.info(request.headers)
+    json_ = await request.json()
+    logger.info(json_)
+    try:
+        verify_signature(await request.body(), os.environ["WEBHOOK_SECRET"], request.headers["x-hub-signature-256"])
+    except HTTPException as e:
+        return e.detail
+
+    # Execute command
+    process = Popen(command[0], shell=True, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    logger.info(f"Executing command: {command[0]}")
+    logger.info(f"stdout: {stdout}")
+    logger.info(f"stderr: {stderr}")
+    return {"status": "success"}
 
 
 @click.command()
-@click.argument("execute", ngargs=1)
 @click.option("--host", default="0.0.0.0", help="Endpoint host")
 @click.option("--port", default=5900, help="Endpoint port")
-def main(execute: str, host: str, port: int):
+@click.option("--name", default="github_webhook_server", help="Name of the server")
+@click.argument("execute")
+def main(host: str, port: int, name: str, execute: str):
     command[0] = execute
+
+    # Config logging
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    file_handler = logging.FileHandler(f"{name}.log", mode="a+")
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger.addHandler(handler)
+
     uvicorn.run(app, host=host, port=port, log_level="info")
 
 
